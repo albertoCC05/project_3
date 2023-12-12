274a9045c160bb5a1c93f36d0449094c44d798ba
albertoCC05 <alberto.cst05@gmail.com>
2023-12-12 11:09:33 +0100

    a

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlayerController : MonoBehaviour
 {
@@ -13,6 +14,29 @@ public class PlayerController : MonoBehaviour
     private bool IsOnTheGround = true;
     [SerializeField] private float intensityJump;
     public bool IsGameOver = false;
+    private Animator playerAnimator;
+    [SerializeField] private ParticleSystem death;
+    [SerializeField] private ParticleSystem particles;
+    private AudioSource playerAudiosource;
+    [SerializeField] private AudioClip jumpClip;
+    [SerializeField] private AudioClip deathClip;
+
+    // FUNCIONES
+
+    private void jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsOnTheGround == true && IsGameOver == false)
+        {
+            playerRigidbody.AddForce(Vector3.up * intensityJump, ForceMode.Impulse);
+            IsOnTheGround = false;
+
+            playerAnimator.SetTrigger("Jump_trig");
+           
+            particles.Stop();
+
+            playerAudiosource.PlayOneShot(jumpClip, 0.7f) ;
+        }
+    }
   
 
     //update
@@ -20,15 +44,14 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         playerRigidbody = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
+        playerAudiosource = GetComponent<AudioSource>();
+        
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && IsOnTheGround == true && IsGameOver == false)
-        {
-            playerRigidbody.AddForce(Vector3.up * intensityJump, ForceMode.Impulse);
-            IsOnTheGround = false;
-        }
+        jump();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -36,11 +59,19 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             IsOnTheGround = true;
+            particles.Play();
         }
 
         if (collision.gameObject.CompareTag("Obstacle"))
         {
             IsGameOver = true;
+            int randomDeath = Random.Range(1, 3);
+            playerAnimator.SetInteger("DeathType_int",randomDeath);
+            playerAnimator.SetBool("Death_b", true);
+            death.Play();
+            particles.Stop();
+            playerAudiosource.PlayOneShot(deathClip, 0.5f);
+
         }
 
        
